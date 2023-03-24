a195daf3fc5f530b003dd6d10e96048d1b3154f9
Nada El-naggar <nadamihelwan@gmail.com>
2023-03-24 03:49:40 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _isupper - uppercase letters
  * @c: char to check
@@ -8,11 +7,10 @@
  * Return: 0 or 1
  */
 
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
