cdad672445a1e47a877d65a7e679c0cc21a63bba
nadaElnggr <nadamihelwan@gmail.com>
2023-06-22 19:36:34 -0700

    t0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all got number
+ * @n:number of sent args
+ * @...:sent integers
+ *
+ * Return: the summed int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i = n;
+	int sum = 0;
+	va_list args;
+
+	if (!n)
+		return (0);
+	vi_start(args, n);
+	while (i)
+	{
+		sum += va_arg(args, int);
+		i -= 1;
+	}
+	va_end(args);
+	return (0);
+}
